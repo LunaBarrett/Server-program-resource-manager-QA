7256363a7113449b21f29875bd0b294983fdddc2
Luna Barrett <luna.barrett@stfc.ac.uk>
2024-03-11 13:25:22 +0000

    created config file

--- config.py
+++ config.py
@@ -1,18 +1,20 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config(object):
     # General Config
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///your_database.db'
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'database', 'userdetails.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Email Configuration (if needed for alerts/notifications)
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
     ADMINS = ['your-email@example.com']  # List of admin email addresses
 
     # Monitoring Configuration (add any additional configuration needed for monitoring)
